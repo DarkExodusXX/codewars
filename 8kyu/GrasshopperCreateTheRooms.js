3f05f398b45d4f365e73830bd9b68b9836ada129
Denis Goncharov <darkexodusxx@gmail.com>
2020-08-07 08:49:55 +0300

    Complete Kata

---
+++ 8kyu/GrasshopperCreateTheRooms.js
@@ -0,0 +1,17 @@
+const rooms = {
+  "1" : {
+    "name" : "bathroom",
+    "description" : "20 min",
+    "completed" : "true"
+  },
+  "2" : {
+    "name" : "kitchen",
+    "description" : "10 min",
+    "completed" : "true"
+  },
+  "3" : {
+    "name" : "bedroom",
+    "description" : "15 min",
+    "completed" : "false"
+  }
+};
