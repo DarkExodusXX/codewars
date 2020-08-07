3787184ca41c1a00ab325bfab1cb4d87dee61e37
Denis Goncharov <darkexodusxx@gmail.com>
2020-08-07 18:27:31 +0300

    Complete Kata

---
+++ 8kyu/ExpressionsMatter.js
@@ -0,0 +1,10 @@
+const expressionMatter = (a, b, c) => {
+  const arr = [];
+  arr.push(a+b+c);
+  arr.push(a*b*c);
+  arr.push(a+b*c);
+  arr.push(a*b+c);
+  arr.push(a*(b+c));
+  arr.push((a+b)*c);
+  return Math.max(...arr);
+};
